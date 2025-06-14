b7a77f885042f7be786679042d5c281a82f531c3
Theodore Keloglou <zf@sirodoht.com>
2025-06-14 04:17:36 +0100

    Fix custom css middleware

--- main/middleware.py
+++ main/middleware.py
@@ -61,7 +61,7 @@ def host_middleware(get_response):
                 raise Http404()
 
             request.account_user = models.User.objects.get(username=request.subdomain)
-            request.custom_css = request.user.custom_css
+            request.custom_css = request.account_user.custom_css
 
             # Redirect to custom urls for cases:
             # * Logged out / anon users
